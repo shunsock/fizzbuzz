595ef1c25a10d85d7874c4f0c9e09d87bf91f19b
Shun <s.tsuchiya.business@gmail.com>
2024-12-08 13:18:50 +0900

    add missing type annotation

--- app/service/router.py
+++ app/service/router.py
@@ -1,5 +1,6 @@
 from app.controller.fizzbuzz import FizzbuzzController
 from app.controller.protocol import ControllerProtocol
+from app.service.parser.parser_output import ParserOutput
 
 
 class Router:
@@ -7,7 +8,7 @@ class Router:
         pass
 
     @staticmethod
-    def route(user_input) -> ControllerProtocol:
+    def route(user_input: ParserOutput) -> ControllerProtocol:
         return FizzbuzzController(
             start = user_input.namespace.start,
             end = user_input.namespace.end
