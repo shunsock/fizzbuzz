0d829f99eadc66b7d9bd6085f2d77327b5c4605a
Shun <s.tsuchiya.business@gmail.com>
2024-12-08 14:35:52 +0900

    remove redundant code

--- app/service/parser/parser.py
+++ app/service/parser/parser.py
@@ -1,5 +1,4 @@
 import argparse
-import io
 
 from app.service.parser.parser_output import ParserOutput
 
@@ -29,15 +28,4 @@ class Parser:
     def parse(self) -> ParserOutput:
         return ParserOutput(
             namespace=self.parser.parse_args(),
-            help_message=self.get_print_help_as_string()
         )
-
-    def get_print_help_as_string(self) -> str:
-        # Create an in-memory file object
-        buffer = io.StringIO()
-
-        # Redirect print_help() output to the buffer
-        self.parser.print_help(file=buffer)
-
-        # Retrieve the content of the buffer as a string
-        return buffer.getvalue()
--- app/service/parser/parser_output.py
+++ app/service/parser/parser_output.py
@@ -5,6 +5,5 @@ from pydantic import BaseModel, ConfigDict
 
 class ParserOutput(BaseModel):
     namespace: Namespace
-    help_message: str
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
